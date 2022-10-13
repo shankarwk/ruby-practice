c0c772f917c0a8d7202b5ada4283e6090df44403
shankarwk <shankar@webkorps.com>
2022-10-13 18:42:25 +0530

    ruby

---
+++ demo.rb
@@ -0,0 +1,8 @@
+a = 6
+begin
+    if a == 6
+        raise "sasa"
+    end    
+rescue => exception
+    puts exception
+end
---
+++ fun.rb
@@ -0,0 +1,6 @@
+def add(r)
+    puts r[:name],r[:age]
+end    
+
+
+add({:name => ["shankar"],:age => 44})
